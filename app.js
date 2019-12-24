9089d082bb8e092e1d78cd53e0a054e1927079da
ShrillShrestha <shrill.shrestha@gmail.com>
2019-12-24 10:08:57 -0600

    Add: contact page

--- app.js
+++ app.js
@@ -11,12 +11,16 @@ var PORT = 3000 || process.env.PORT;
 app.locals.fontId = process.env.fontawesome;
 
 app.get("/", function(req, res){
-    res.render("index");
+    res.render("pages/index");
 });
 
 app.get("/about", function(req, res){
-    res.render("about");
-})
+    res.render("pages/about");
+});
+
+app.get("/contact", function(req, res){
+    res.render("pages/contact");
+});
 
 app.listen(PORT, function(req, res){
     console.log("Running!!!");
